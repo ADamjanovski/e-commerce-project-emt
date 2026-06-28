421811585f28b92c6d73504470269652d2ac2bfc
Andrej Damjanovski <andrej-damjanovski@hotmail.com>
2026-06-28 12:41:47 +0200

    Change redisc serialising config

--- backend/src/main/java/com/backend/configuration/CacheConfig.java
+++ backend/src/main/java/com/backend/configuration/CacheConfig.java
@@ -5,6 +5,8 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
 
@@ -17,6 +19,9 @@ public class CacheConfig {
         return builder -> builder
                 .withCacheConfiguration("recommendations",
                         RedisCacheConfiguration.defaultCacheConfig()
+                                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                                        new GenericJackson2JsonRedisSerializer()
+                                ))
                                 .entryTtl(Duration.ofDays(7)) // expire after 7 days
                 );
     }
